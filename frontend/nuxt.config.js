683eb213566da30a7d81a4870ebea1a40a3c217d
si0005hp <si0005hp@gmail.com>
2019-08-11 13:51:34 +0900

    carried frontend srcs

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,54 @@
+const config = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'nuxt-auth0',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: '//cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css'
+      }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  mode: 'spa',
+  plugins: ['~/plugins/auth0.js'],
+  auth0: {
+    domain: 'si0005hp-dev1.auth0.com',
+    clientID: 'ij432Da6kzxbArLbqRPsPaB4TXOFs86Y'
+  },
+}
+
+module.exports = config
---
+++ frontend/pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="container">
+    <div>
+      <div v-if="loggedIn()" class="content">
+        <h2>ログイン中です</h2>
+        <nuxt-link class="button is-warning" to="/logout">
+          <span class="icon">
+            <i class="fa fa-sign-out"></i>
+          </span>
+          <span>Logout</span>
+        </nuxt-link>
+      </div>
+
+      <div v-if="!loggedIn()" class="content">
+        <h2>ログインしてください</h2>
+        <nuxt-link class="button is-primary" to="/login">
+          <span class="icon">
+            <i class="fa fa-sign-in"></i>
+          </span>
+          <span>Login</span>
+        </nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<script>
+export default {
+  methods: {
+    loggedIn() {
+      return this.$auth0.isAuthenticated();
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
